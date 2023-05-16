ce15e88cc55fa4bdb6d8cc1261f8935d5bc1ef73
andrew ipsen <andy.ipsen@gmail.com>
2023-05-16 16:33:53 -0700

    company endpoints completed

--- api/main.py
+++ api/main.py
@@ -1,19 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-<<<<<<< HEAD
-from routers import accounts, companies
+from routers import accounts, companies, positions
 
 app = FastAPI()
 app.include_router(accounts.router)
 app.include_router(companies.router)
-=======
-from routers import accounts, positions
-
-app = FastAPI()
-app.include_router(accounts.router)
 app.include_router(positions.router)
->>>>>>> main
 
 app.add_middleware(
     CORSMiddleware,

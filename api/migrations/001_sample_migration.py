069712e46c2023da07b47a382aa5aaa636aced46
Mohammed Rahman <brohammedr@gmail.com>
2023-05-15 17:38:38 -0700

    account table created

--- api/migrations/001_sample_migration.py
+++ api/migrations/001_sample_migration.py
@@ -1,38 +1,20 @@
 steps = [
     [
-        # "Up" SQL statement
+        ##Create the table
         """
-        CREATE TABLE dummy (
+        CREATE TABLE account (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
+            email VARCHAR(200) NOT NULL UNIQUE,
+            password VARCHAR(20) NOT NULL,
+            position_id INT NULL
         );
+
         """,
-        # "Down" SQL statement
+        ##Drop the table
         """
-        DROP TABLE dummy;
-        """
-    ],
-    [
-        # "Up" SQL statement
-        """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE big_dummy;
+        DROP TABLE account;
         """
     ]
 ]

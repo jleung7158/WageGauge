26b96254f82ed453ac79b42a369feb853b35330e
Mohammed Rahman <brohammedr@gmail.com>
2023-05-16 21:54:27 -0700

    Authorization is working, create account end point working, account table working

--- api/routers/accounts.py
+++ api/routers/accounts.py
@@ -1,16 +1,52 @@
-from fastapi import APIRouter, Depends
-from queries.accounts import(
+from fastapi import (
+    Depends,
+    HTTPException,
+    status,
+    Response,
+    APIRouter,
+    Request,
+)
+from jwtdown_fastapi.authentication import Token
+from authenticator import authenticator
+
+from pydantic import BaseModel
+
+from queries.accounts import (
     AccountIn,
     AccountOut,
-    AccountRepository
+    AccountOutWithPassword,
+    AccountRepository,
+    DuplicateAccountError,
 )
 
-router= APIRouter()
-depend= Depends()
+class AccountForm(BaseModel):
+    username: str
+    password: str
 
-@router.post("/accounts", response_model= AccountOut)
-def create_account(
-    account: AccountIn,
-    repo: AccountRepository=depend
+class AccountToken(Token):
+    account: AccountOut
+
+class HttpError(BaseModel):
+    detail: str
+
+router = APIRouter()
+
+
+@router.post("/api/accounts", response_model=AccountToken | HttpError)
+async def create_account(
+    info: AccountIn,
+    request: Request,
+    response: Response,
+    repo: AccountRepository = Depends(),
 ):
-    return repo.create(account)
+    hashed_password = authenticator.hash_password(info.password)
+    try:
+        account = repo.create(info, hashed_password)
+    except DuplicateAccountError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Cannot create an account with those credentials",
+        )
+    form = AccountForm(username=info.email, password=info.password)
+    token = await authenticator.login(response, request, form, repo)
+    return AccountToken(account=account, **token.dict())

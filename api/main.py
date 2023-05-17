af62090824f1a29ac0d9861b7bdabc79953f3b66
James Leung <jamesleung7158@gmail.com>
2023-05-16 17:31:38 -0700

    forgot one merge conflict

--- api/main.py
+++ api/main.py
@@ -1,18 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-<<<<<<< HEAD
-from routers import accounts, positions
-
-app = FastAPI()
-app.include_router(accounts.router)
-=======
 from routers import accounts, companies, positions
 
 app = FastAPI()
 app.include_router(accounts.router)
 app.include_router(companies.router)
->>>>>>> main
 app.include_router(positions.router)
 
 app.add_middleware(

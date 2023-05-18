0745c4a48df145d3b92734c4c572b0b30027fee2
andrew ipsen <andy.ipsen@gmail.com>
2023-05-18 15:11:16 -0700

    errors

--- api/migrations/003_employee_migrations.py
+++ api/migrations/003_employee_migrations.py
@@ -9,11 +9,7 @@ steps = [
             account_id INT NULL,
             company_id INT NULL,
             salary INT,
-<<<<<<< HEAD
-            location VARCHAR(100) NOT NULL
-=======
             location VARCHAR(1000) NOT NULL
->>>>>>> 501663937f5de7105de365fb103d41d73a9a0d61
         );
         """,
         ## Drop the Table

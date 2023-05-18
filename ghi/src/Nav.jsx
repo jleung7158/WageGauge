5ba2ee8825f723aa6ddffd3627d9ef3380e4020e
andrew ipsen <andy.ipsen@gmail.com>
2023-05-18 14:33:56 -0700

    nav conflicts squashed

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -3,43 +3,56 @@ import { NavLink } from 'react-router-dom';
 
 
 function Nav() {
-  return (
+	return (
     <div className="container-flex flex-row w-screen p-2 mb-8 color-bg">
-      <div
-        className="
-        color-bg"
-        >
-          <div
-            className="
-          px-2 rounded
-          color-bg
-          "
-          >
-
-              <div className="relative flex h-16 items-center justify-between">
-              <div>
-                <NavLink to="positions"
-                className="text-gray-50
+    <div
+			className="
+      color-bg"
+		>
+			<div
+				className="
+        px-2 rounded
+        color-bg
+        "
+			>
+				<div className="relative flex h-16 items-center justify-between">
+					<NavLink
+						to="positions"
+						className="text-gray-50
+						hover:bg-gray-700
+						hover:text-white
+						block rounded-md px-3 py-2
+						text-base font-medium"
+					>
+						Positions
+					</NavLink>
+				</div>
+				<div>
+					<NavLink
+						to="companies"
+						className="text-gray-50
                 hover:bg-gray-700
-                hover:text-white
-                block rounded-md px-3 py-2
+                hover:text-white block
+                rounded-md px-3 py-2
                 text-base font-medium"
-                >Positions</NavLink>
-              </div>
-                <div >
-                  <NavLink to="companies"
-                  className="text-gray-50
-                  hover:bg-gray-700
-                  hover:text-white block
-                  rounded-md px-3 py-2
-                  text-base font-medium">
-                    Companies</NavLink>
-                </div>
-          </div>
-          </div>
-      </div>
-    </div>
-  );
+					>
+						Companies
+					</NavLink>
+				</div>
+				<div>
+					<NavLink
+            to="signup"
+            className="
+                text-gray-50
+                hover:bg-gray-700
+                hover:text-white block
+                rounded-md px-3 py-2
+                text-base font-medium">SignUp</NavLink>
+				</div>
+			</div>
+		</div>
+		</div>
+	);
 }
 
 export default Nav;

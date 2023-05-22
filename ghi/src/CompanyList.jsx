22db4a518b0a7d73b815ef391db5bf3fb70bc7f5
andrew ipsen <andy.ipsen@gmail.com>
2023-05-22 15:24:11 -0700

    company list edits

--- ghi/src/CompanyList.jsx
+++ ghi/src/CompanyList.jsx
@@ -141,9 +141,8 @@ function CompanyList() {
                                 <div>
                                     {/* the Search Bar */}
                                     <form onSubmit={handleSearch}>
-                                        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-                                            Search
-                                        </label>
+                                        {/* <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+                                        </label> */}
                                         <div className="relative">
                                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                             <svg

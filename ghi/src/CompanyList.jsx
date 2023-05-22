2e4a24d78b04cb3c869584beeb1abc40c6910e8a
andrew ipsen <andy.ipsen@gmail.com>
2023-05-22 13:47:07 -0700

    created footer code in a separate file

--- ghi/src/CompanyList.jsx
+++ ghi/src/CompanyList.jsx
@@ -182,6 +182,7 @@ function CompanyList() {
                                     font-bold
                                     text-4xl
                                     text-center'>COMPANY LIST</h1>
+                                    {/* mapping by company first */}
                                     {companies.map((company) => (
                                         <div key={company.id}>
                                         <h2 className='
@@ -194,6 +195,7 @@ function CompanyList() {
                                         font-bold
                                         text-3xl
                                         px-10'>{company.name}</h2>
+
                                         <table className="table-fixed w-full bg-gray-100 rounded-md border-separate border-spacing-2 border border-white">
                                             <thead>
                                             <tr className="text-xl font-bold font-warownia">
@@ -202,7 +204,7 @@ function CompanyList() {
                                             </tr>
                                             </thead>
                                             <tbody>
-
+                                            {/* mapping positions by their company id */}
                                             {getPositionsByCompanyId(company.id).map((position, index) => (
                                                 <tr key={position.id}className="w-full ml-5 font-warownia font-bold text-2xl">
                                                     <td className="px-10 py-10 bg-gray-50 rounded-md border border-white">
@@ -213,6 +215,7 @@ function CompanyList() {
                                                     </td>
                                                 {/* <td className="px-10 py-10 bg-gray-50 rounded-md border border-white text-center">{position.name}</td>*/}
                                                 <td className="px-10 py-10 bg-gray-50 rounded-md border border-white text-center">Salary TBD</td>
+                                                {/* if the index of the position is the last one in the list, include the learn more button  */}
                                                 {index === getPositionsByCompanyId(company.id).length - 1 && (
                                                 <td className="px-20 py-5"><Link onClick={`/companies/${company.id}`}>
                                                     <button
@@ -244,110 +247,6 @@ function CompanyList() {
 				</div>
 			</div>
 		</div>
-    {/* footer */}
-    <footer className="flex bg-white dark:bg-gray-900">
-    <div className="container px-6 py-12 mx-auto">
-        <div className="text-center">
-            <h2 className="text-2xl font-semibold text-gray-800 dark:text-white">Let’s get started on something great</h2>
-
-            <p className="max-w-md mx-auto mt-2 text-gray-500 dark:text-gray-400">Join over 4,000+ startups already growing with Meraki UI.</p>
-
-            <div class="flex flex-col mt-6 sm:flex-row sm:items-center sm:justify-center">
-                <button className="w-full px-5 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-wageblue rounded-md sm:mx-2 sm:order-2 sm:w-auto hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">Get started</button>
-            </div>
-        </div>
-
-        <div class="grid grid-cols-2 gap-6 mt-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-            <div>
-                <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">Product</h3>
-
-                <div className="flex flex-col items-start mt-4 space-y-4">
-                    {/* <a href="#" className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Overview</a>
-                    <a href="#" className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Features</a>
-                    <a href="#" className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Solutions</a>
-                    <a href="#" className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Tutorials</a>
-                    <a href="#" className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Pricing</a>
-                    <a href="#" className="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Releases</a> */}
-                </div>
-            </div>
-
-            <div>
-                <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">Company</h3>
-
-                <div className="flex flex-col items-start mt-4 space-y-4">
-                    {/* <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">About us</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Careers</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Press</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">News</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Media kit</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Contact</a> */}
-                </div>
-            </div>
-
-            <div>
-                <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">Resources</h3>
-
-                <div className="flex flex-col items-start mt-4 space-y-4">
-                    {/* <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Blog</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Newsletter</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Events</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Help center</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Tutorials</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Supports</a> */}
-                </div>
-            </div>
-
-            <div>
-                <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">Use cases</h3>
-
-                <div className="flex flex-col items-start mt-4 space-y-4">
-                    {/* <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Startups</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Enterprise</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Government</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Saas</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Marketplaces</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Ecommerce</a> */}
-                </div>
-            </div>
-
-            <div>
-                <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">Social</h3>
-
-                <div className="flex flex-col items-start mt-4 space-y-4">
-                    {/* <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Twitter</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">LinkedIn</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Github</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Facebook</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">AngelList</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Dribble</a> */}
-                </div>
-            </div>
-
-            <div>
-                <h3 className="text-sm font-medium text-gray-500 dark:text-gray-400">Legal</h3>
-
-                <div className="flex flex-col items-start mt-4 space-y-4">
-                    {/* <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Terms</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Privacy</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Cookies</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Licenses</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Settings</a>
-                    <a href="#" class="text-gray-700 transition-colors duration-200 dark:text-gray-200 dark:hover:text-blue-400 hover:underline hover:text-blue-600">Contact</a> */}
-                </div>
-            </div>
-        </div>
-
-        <hr clasNames="my-6 border-gray-200 md:my-10 dark:border-gray-700"/>
-
-        <div className="flex flex-col items-center justify-between sm:flex-row">
-            {/* <a href="#">
-                <img className="w-auto h-7" src="./img/icon.png" alt=""/>
-            </a> */}
-
-            <p className="mt-4 text-sm text-gray-500 sm:mt-0 dark:text-gray-300">© Copyright 2023. All Rights Reserved.</p>
-        </div>
-    </div>
-</footer>
         </>
 	);
 }

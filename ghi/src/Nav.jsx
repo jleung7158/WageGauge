a131fc993939229aa8b33813c8c58928dda2ef89
Mohammed Rahman <brohammedr@gmail.com>
2023-05-30 10:38:03 -0700

    Created a modal popout window for Login feature

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -1,43 +1,53 @@
-import { NavLink } from "react-router-dom";
-import HomePageIcon from "./img/icon.png";
-import Switcher from "./components/Switcher";
-import Banner from "./components/Banner";
-
+import { NavLink } from 'react-router-dom';
+import HomePageIcon from './img/icon.png';
+import Switcher from './components/Switcher';
+import Banner from './components/Banner';
+import LoginForm from './Login';
+import { useState } from 'react';
 
 function Nav() {
+	const [showLoginForm, setShowLoginForm] = useState(false);
+	const handleLoginClick = () => {
+		setShowLoginForm(true);
+	};
 
+	const handleCloseLoginForm = () => {
+		setShowLoginForm(false);
+	};
 
-  return (
-    <>
-      {/* the pop up to sign in */}
-      <Banner/>
-      {/* the background of the nav */}
-      <div className="
+	return (
+		<>
+			{/* the pop up to sign in */}
+			<Banner />
+			{/* the background of the nav */}
+			<div
+				className="
           container-flex flex-row p-2
           bg-white
           dark:bg-moredark
-          ">
-        {/* makes the buttons horizontal */}
-        <div className="flex px-2 rounded space-x-4 mt-4 mb-5 justify-between">
-          {/* the buttons on the left*/}
-          <div className="flex flex-row space-x-4 justify-start">
-            <div>
-              <NavLink
-                to="companies"
-                className="
+          "
+			>
+				{/* makes the buttons horizontal */}
+				<div className="flex px-2 rounded space-x-4 mt-4 mb-5 justify-between">
+					{/* the buttons on the left*/}
+					<div className="flex flex-row space-x-4 justify-start">
+						<div>
+							<NavLink
+								to="companies"
+								className="
                 hover:opacity-25
                 transition-opacity
                 block rounded-full px-5 py-4
 
                 "
-              >
-                <img src={HomePageIcon} alt="Homepage" className="w-12 h-12" />
-              </NavLink>
-            </div>
-            <div className="mt-5">
-              <NavLink
-                to="companies"
-                className="text-gray-50
+							>
+								<img src={HomePageIcon} alt="Homepage" className="w-12 h-12" />
+							</NavLink>
+						</div>
+						<div className="mt-5">
+							<NavLink
+								to="companies"
+								className="text-gray-50
                 hover:bg-gray-100
                 hover:text-wageblue block
                 block rounded-full px-5 py-3
@@ -50,14 +60,14 @@ function Nav() {
                 dark:hover:shadow-slate-500/20
                 dark:hover:text-gray-300
                 "
-              >
-                Dashboard
-              </NavLink>
-            </div>
-            <div className="mt-5">
-              <NavLink
-                to="positions"
-                className="
+							>
+								Dashboard
+							</NavLink>
+						</div>
+						<div className="mt-5">
+							<NavLink
+								to="positions"
+								className="
                   text-gray-50
                   hover:bg-gray-100
                   hover:text-wageblue
@@ -71,24 +81,26 @@ function Nav() {
                   dark:hover:shadow-slate-500/20
                   dark:hover:text-gray-300
                   "
-              >
-                Positions
-              </NavLink>
-            </div>
-          </div>
-          {/* nav buttons on the right side* */}
-          {/* </div> */}
-          <div className="flex flex-row space-x-4 justify-start">
-            <div className="mt-6
+							>
+								Positions
+							</NavLink>
+						</div>
+					</div>
+					{/* nav buttons on the right side* */}
+					{/* </div> */}
+					<div className="flex flex-row space-x-4 justify-start">
+						<div
+							className="mt-6
                     rounded-full px-3
                     dark:shadow-lg
-                    dark:hover:shadow-slate-500/20">
-              <Switcher/>
-            </div>
-            <div className="mt-5">
-              <NavLink
-                to="about"
-                className="
+                    dark:hover:shadow-slate-500/20"
+						>
+							<Switcher />
+						</div>
+						<div className="mt-5">
+							<NavLink
+								to="about"
+								className="
                     text-gray-50
                     hover:bg-gray-100
                     hover:text-wageblue block
@@ -101,14 +113,14 @@ function Nav() {
                     dark:shadow-inner
                     dark:hover:shadow-slate-500/20
                     "
-              >
-                About Us
-              </NavLink>
-            </div>
-            <div className="mt-5">
-              <NavLink
-                to="signup"
-                className="
+							>
+								About Us
+							</NavLink>
+						</div>
+						<div className="mt-5">
+							<NavLink
+								to="#"
+								className="
                     text-gray-50
                     hover:bg-gray-100
                     hover:text-wageblue block
@@ -117,22 +129,29 @@ function Nav() {
                     text-base font-bold
 
                     dark:text-wageblue
-                    dark:bg-moredark
-                    dark:shadow-inner
-                    dark:hover:shadow-slate-500/20
-                    "
-              >
-                Login
-              </NavLink>
-            </div>
+                    dark:bg-darkblue
+                    hover:dark:bg-darkgray"
+								onClick={handleLoginClick}
+							>
+								Login
+							</NavLink>
+						</div>
+					</div>
 
-        </div>
-        </div>
-      </div>
-      {/* bottom space */}
-      {/* <div className=' pb-5' ></div> */}
-    </>
-  );
+					{showLoginForm && (
+						<div className="fixed inset-0 flex items-center justify-center z-50">
+							<div className="fixed inset-0 bg-black opacity-50"></div>
+							<div className="relative z-10">
+								<LoginForm onClose={handleCloseLoginForm} />
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+			{/* bottom space */}
+			{/* <div className=' pb-5' ></div> */}
+		</>
+	);
 }
 
 export default Nav;

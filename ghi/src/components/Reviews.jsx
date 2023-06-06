e212cdc85bc4f1fa6470b3f8f563f24fbf30a468
Andrew Ipsen <andy.ipsen@gmail.com>
2023-06-06 17:53:09 +0000

    profile dropdown fixed, closes 52

--- ghi/src/components/Reviews.jsx
+++ ghi/src/components/Reviews.jsx
@@ -4,10 +4,10 @@ import React from "react";
 export default function Reviews() {
     return (
         <div className="
-        grid mb-8 shadow-sm md:mb-12 md:grid-cols-2 dark:bg-moredark
+        grid mb-8 shadow-sm md:mb-12 md:grid-cols-2 dark:bg-moredark rounded-full p-5
         ">
-        <figure className="flex flex-col items-center justify-center p-8 text-center bg-white dark:bg-moredark">
-            <div className="bg-white rounded-full h-[150px] w-[150px] shadow-lg dark:bg-darkblue">
+        <figure className="flex flex-col rounded-full items-center justify-center p-8 text-center bg-white bg-[#5d697a] dark:bg-[#5d697a]">
+            <div className="bg-white rounded-full h-[150px] w-[150px] shadow-lg bg-white dark:bg-darkblue">
 
             </div>
             </figure>
